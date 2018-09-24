a7891938266c1640f84c08737f4506f188c1eee5
Aswin Bhim Nath <aswismail@gmail.com>
2018-09-24 22:16:22 +0530

    Sign Up Activity

--- app/src/main/java/com/andromeeda/project/firebaselogin/MainActivity.java
+++ app/src/main/java/com/andromeeda/project/firebaselogin/MainActivity.java
@@ -2,12 +2,16 @@ package com.andromeeda.project.firebaselogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button login, signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.login_login);
+        signup = findViewById(R.id.login_signup);
+        
     }
 }
