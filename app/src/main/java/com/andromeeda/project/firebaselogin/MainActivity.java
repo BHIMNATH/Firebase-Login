05ad29f4cc55a81a07996a9d48aeef37e8cdd31c
Aswin Bhim Nath <aswismail@gmail.com>
2018-09-24 22:39:47 +0530

    SignUp Event Listners

--- app/src/main/java/com/andromeeda/project/firebaselogin/MainActivity.java
+++ app/src/main/java/com/andromeeda/project/firebaselogin/MainActivity.java
@@ -1,7 +1,9 @@
 package com.andromeeda.project.firebaselogin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         login = findViewById(R.id.login_login);
         signup = findViewById(R.id.login_signup);
-        
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/andromeeda/project/firebaselogin/SignUp.java
+++ app/src/main/java/com/andromeeda/project/firebaselogin/SignUp.java
@@ -1,13 +1,28 @@
 package com.andromeeda.project.firebaselogin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
-
+    Button signUp,login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        signUp = findViewById(R.id.signup_signup);
+        login = findViewById(R.id.signup_login);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
