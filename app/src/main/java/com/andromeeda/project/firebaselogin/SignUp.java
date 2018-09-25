f75c3c5aab5b58e4f856fd56a94ce8a6270ffae9
Aswin Bhim Nath <aswismail@gmail.com>
2018-09-25 23:39:28 +0530

    Firbase Authentication

--- app/src/main/java/com/andromeeda/project/firebaselogin/SignUp.java
+++ app/src/main/java/com/andromeeda/project/firebaselogin/SignUp.java
@@ -3,11 +3,15 @@ package com.andromeeda.project.firebaselogin;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
     Button signUp,login;
+    EditText mail,pswd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +19,8 @@ public class SignUp extends AppCompatActivity {
 
         signUp = findViewById(R.id.signup_signup);
         login = findViewById(R.id.signup_login);
+        mail = findViewById(R.id.signup_email);
+        pswd = findViewById(R.id.signup_pswd);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +28,25 @@ public class SignUp extends AppCompatActivity {
                 Intent intent = new Intent(SignUp.this, MainActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mail.getText().toString().trim();
+                String password = pswd.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
