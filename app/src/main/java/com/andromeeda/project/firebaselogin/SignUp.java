bdf68ff066d05a5b2e0dc4c2874ef5f7b4341400
Aswin Bhim Nath <aswismail@gmail.com>
2018-09-26 15:33:22 +0530

    Firbase Authentication Code

--- app/src/main/java/com/andromeeda/project/firebaselogin/SignUp.java
+++ app/src/main/java/com/andromeeda/project/firebaselogin/SignUp.java
@@ -1,6 +1,7 @@
 package com.andromeeda.project.firebaselogin;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,9 +10,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignUp extends AppCompatActivity {
     Button signUp,login;
     EditText mail,pswd;
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +54,21 @@ public class SignUp extends AppCompatActivity {
                 if (password.length() < 6) {
                     Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
                 }
+
+                firebaseAuth.createUserWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(SignUp.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                if(!task.isSuccessful()){
+                                    Toast.makeText(SignUp.this, "Authentication failed." + task.getException(),Toast.LENGTH_LONG).show();
+                                }
+                                else {
+                                    startActivity(new Intent(SignUp.this,MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
             }
         });
     }
