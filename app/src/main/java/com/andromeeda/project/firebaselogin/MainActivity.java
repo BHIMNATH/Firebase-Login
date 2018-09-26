2b0052c02b0af0aa03bd9adad06a8a6f5a2474ac
Aswin Bhim Nath <aswismail@gmail.com>
2018-09-26 22:38:11 +0530

    Login using Firebase

--- app/src/main/java/com/andromeeda/project/firebaselogin/MainActivity.java
+++ app/src/main/java/com/andromeeda/project/firebaselogin/MainActivity.java
@@ -1,6 +1,7 @@
 package com.andromeeda.project.firebaselogin;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -10,6 +11,9 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
         pswd = findViewById(R.id.login_pswd);
         progressBar = findViewById(R.id.progressBar);
         firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) { // If already logged in
+            startActivity(new Intent(MainActivity.this, Home.class));
+            finish();
+        }
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,14 +44,30 @@ public class MainActivity extends AppCompatActivity {
 
                 if (TextUtils.isEmpty(email)) {
                     Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
-                    return;
                 }
                 if (TextUtils.isEmpty(password)) {
                     Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
-                    return;
                 }
-                Intent intent = new Intent(MainActivity.this, Home.class);
-                startActivity(intent);
+                progressBar.setVisibility(View.VISIBLE);
+                firebaseAuth.signInWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if(!task.isSuccessful()){
+                                    if(password.length() < 6){
+                                        mail.setError("Password too short, enter minimum 6 characters!");
+                                    }
+                                    else {
+                                        Toast.makeText(MainActivity.this, "Login failed!", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                                else {
+                                    startActivity(new Intent(MainActivity.this, Home.class));
+                                    finish();
+                                }
+                            }
+                        });
             }
         });
 
